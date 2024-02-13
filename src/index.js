11375d5e75aceaf2780523e4ec8261658d1f4bd8
HwangIsAce <esteen196@naver.com>
2024-02-13 22:26:13 +0900

    feat first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000
+
+app.get('/', (req, res)=>{
+    res.send('Hello world!');
+});
+
+app.listen(port, ()=>{
+    console.log('app listening on port ${port}, point your browser at http://localhost:3000');
+});

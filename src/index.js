3dc988c087fd9800f1427c47c81423c549b8513b
HwangIsAce <esteen196@naver.com>
2024-02-14 12:44:48 +0900

    refactor video streaming file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
-const port = 3000
+const port = 3000;
 
-app.get('/', (req, res)=>{
-    res.send('Hello world!');
+app.get("/video", (req, res)=>{
+    
+    const path = "/home/jaesung/jaesung/development/video_streamer/videos/SampleVideo_1280x720_1mb.mp4";
+    fs.stat(path, (err, stats) =>{
+        if (err) {
+            console.error("An error occured");
+            res.sendStatus(500);
+            return;
+        }
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
+        });
+        fs.createReadStream(path).pipe(res); // createReadStream -> Reading large files in chunks // pipe -> The contents of the file can be delivered as an HTTP response
+    });
 });
 
 app.listen(port, ()=>{
-    console.log('app listening on port ${port}, point your browser at http://localhost:3000');
+    console.log('app listening on port ${port}, point your browser at http://localhost:3000/video');
 });
